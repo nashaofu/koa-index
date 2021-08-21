a135b09aef65090e9c97e7bbdf1483602af608d2
nashaofu <diaocheng@outlook.com>
2021-08-21 16:46:42 +0800

    feat: 支持文件

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,29 @@ import sendFile from './sendFile'
 import sendDirectory from './sendDirectory'
 
 export interface IOpts {
-  index?: string
+  index?: string | boolean
   maxage?: number
+  lastModified?: boolean
+  etag?: boolean
   hidden?: boolean
 }
 
-export default function (root: string, opts: IOpts = {}): Middleware {
+export default function (iRoot: string, iOpts?: IOpts): Middleware {
+  const root = path.resolve(iRoot)
+
+  const opts = {
+    index: iOpts?.index ?? 'index.html',
+    maxage: iOpts?.maxage ?? 0,
+    lastModified: iOpts?.lastModified ?? true,
+    etag: iOpts?.etag ?? true,
+    hidden: iOpts?.hidden ?? true
+  }
+
   return async function (ctx: Context, next: Next): Promise<void> {
+    // only accept GET and HEAD
+    if (ctx.method !== 'GET' && ctx.method !== 'HEAD') {
+      return next()
+    }
     if (ctx.body != null || ctx.status !== 404) {
       return next()
     }
@@ -19,8 +35,12 @@ export default function (root: string, opts: IOpts = {}): Middleware {
     const pathname = decodeURIComponent(ctx.path)
     const filename = path.join(root, pathname)
 
+    if (!filename.startsWith(root)) {
+      return next()
+    }
+
     // 判断是否为隐藏文件
-    if (!opts.hidden && path.basename(filename)[0] === '.') {
+    if (opts.hidden && path.basename(filename)[0] === '.') {
       return next()
     }
 
@@ -30,17 +50,25 @@ export default function (root: string, opts: IOpts = {}): Middleware {
       await sendFile(filename, ctx, {
         stat,
         maxage: opts.maxage,
+        lastModified: opts.lastModified,
+        etag: opts.etag,
         hidden: opts.hidden
       })
     } else {
-      if (opts.index) {
+      if (opts.index && typeof opts.index === 'string') {
         const indexFileName = path.join(filename, opts.index)
-        const indexFileStat = await fs.stat(indexFileName)
+        let indexFileStat
+        try {
+          indexFileStat = await fs.stat(indexFileName)
+        } catch (err) {}
+
         // 只处理文件的情况，如果是文件夹则还是显示filename文件夹的内容
-        if (indexFileStat.isFile()) {
+        if (indexFileStat && indexFileStat.isFile()) {
           await sendFile(filename, ctx, {
             stat: indexFileStat,
             maxage: opts.maxage,
+            lastModified: opts.lastModified,
+            etag: opts.etag,
             hidden: opts.hidden
           })
         } else {
--- src/sendFile.ts
+++ src/sendFile.ts
@@ -1,22 +1,70 @@
 import type { Context } from 'koa'
 import fs, { Stats } from 'fs-extra'
-// import readFile from './readFile'
 import path from 'path'
+import etag from 'etag'
+import rangeParser from 'range-parser'
 
 export interface SFOpts {
   stat: Stats
-  maxage?: number
-  hidden?: boolean
+  maxage: number
+  lastModified: boolean
+  etag: boolean
+  hidden: boolean
 }
 
 export default async function sendFile (filename: string, ctx: Context, opts: SFOpts): Promise<void> {
   const stat = opts.stat
-  const maxage = (opts.maxage / 1000) | 0
-
-  ctx.set('Content-Length', stat.size)
-  ctx.set('Last-Modified', stat.mtime.toUTCString())
-  ctx.set('Cache-Control', `max-age=${maxage}`)
+  const maxage = (opts.maxage ?? 0 / 1000) | 0
 
   ctx.type = path.extname(filename)
-  ctx.body = fs.createReadStream(filename)
+
+  if (!ctx.response.get('Cache-Control')) {
+    ctx.set('Cache-Control', `public, max-age=${maxage}`)
+  }
+  if (opts.lastModified && !ctx.response.get('Last-Modified')) {
+    ctx.set('Last-Modified', stat.mtime.toUTCString())
+  }
+  if (opts.etag && !ctx.response.get('ETag')) {
+    ctx.set('ETag', etag(stat, { weak: true }))
+  }
+
+  // range header支持
+  const range = ctx.get('range')
+
+  if (typeof range === 'string' && range) {
+    const ranges = rangeParser(stat.size, range, {
+      combine: true
+    })
+
+    // unsatisfiable
+    if (ranges === -1) {
+      // Content-Range
+      ctx.set('Content-Range', `bytes */${stat.size}`)
+
+      // 416 Requested Range Not Satisfiable
+      ctx.throw(416)
+      return
+    }
+
+    // valid (syntactically invalid/multiple ranges are treated as a regular response)
+    if (ranges !== -2 && ranges.length === 1) {
+      // Content-Range
+      ctx.status = 206
+      const start = ranges[0].start
+      const end = ranges[0].end
+      ctx.set('Content-Range', `${ranges.type} ${start}-${end}/${stat.size}`)
+      ctx.body = fs.createReadStream(filename, {
+        start,
+        end
+      })
+    }
+  } else if (
+    (opts.lastModified || opts.etag) &&
+    (ctx.get('If-Modified-Since') || ctx.get('If-None-Match')) &&
+    ctx.stale
+  ) {
+    ctx.status = 304
+  } else {
+    ctx.body = fs.createReadStream(filename)
+  }
 }
